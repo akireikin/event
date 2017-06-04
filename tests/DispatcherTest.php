a68029073421c9349c22f23871bf4c2edc8216cc
Aleksei Akireikin <opexus@gmail.com>
2017-06-04 11:04:48 +0000

    Apply fixes from StyleCI

--- tests/DispatcherTest.php
+++ tests/DispatcherTest.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the Event package.
+ *
+ * (c) Aleksei Akireikin <opexus@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Akireikin\EventTest;
 
 use Akireikin\Event\Dispatcher;
